49e94935f7abaf6583e8e0e4ba3c73f268e3a486
ficheles <rafaelfideles@live.com>
2021-03-22 21:24:14 -0300

    Ajustar path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gitpage-vue/'
+    : '/',
   outputDir: 'docs'
 }

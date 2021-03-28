3ed608300bccdf6cc92ca4c7ab6c50f158c88db3
ficheles <rafaelfideles@live.com>
2021-03-27 21:28:07 -0300

    Updates

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production',
-  outputDir: 'docs',
-  productionSourceMap: false,
-  // publicPath: '/gitpage-vue/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/gitpage-vue/' : '/',
-  runtimeCompiler: true,
-}
